e9732c0aaa2ba3fa0fa062837f44337bc54a1730
Lonel Ice <ijumrz@gmail.com>
2021-09-20 12:27:14 +0300

    refactor: improved readability isStuck.ts

--- src/render/isStuck.ts
+++ src/render/isStuck.ts
@@ -7,15 +7,19 @@ export const isStuck = (rectangles: Rectangle[], select: Rectangle, intervalGlui
     let intervals = calculateIntervals(select, rectangles);
     rectangles.forEach((rect: Rectangle, index: number): void => {
         if (select !== rect) {
-            if (intervals[index].left < intervalGluing && intervals[index].left > 0 && intervals[index].bot < 0 && intervals[index].top < 0) {
+            const stickToLeft: boolean = intervals[index].left < intervalGluing && intervals[index].left > 0 && intervals[index].bot < 0 && intervals[index].top < 0;
+            const stickToRight: boolean = intervals[index].right < intervalGluing && intervals[index].top < 0 && intervals[index].bot < 0 && intervals[index].right > 0;
+            const stickToTop: boolean = intervals[index].top < intervalGluing && intervals[index].left < 0 && intervals[index].top > 0 && intervals[index].right < 0;
+            const stickToBot: boolean = intervals[index].bot < intervalGluing && intervals[index].left < 0 && intervals[index].bot > 0 && intervals[index].right < 0;
+            if (stickToLeft) {
                 select.x = rect.x + rect.width + borderWidth / 2;
                 relocateToggle(true);
-            } else if (intervals[index].right < intervalGluing && intervals[index].top < 0 && intervals[index].bot < 0 && intervals[index].right > 0) {
+            } else if (stickToRight) {
                 select.x = rect.x - select.width - borderWidth / 2;
                 relocateToggle(true);
-            } else if (intervals[index].top < intervalGluing && intervals[index].left < 0 && intervals[index].top > 0 && intervals[index].right < 0) {
+            } else if (stickToTop) {
                 select.y = rect.y + rect.height + borderWidth / 2;
-            } else if (intervals[index].bot < intervalGluing && intervals[index].left < 0 && intervals[index].bot > 0 && intervals[index].right < 0) {
+            } else if (stickToBot) {
                 select.y = rect.y - select.height - borderWidth / 2;
                 relocateToggle(true);
             }
